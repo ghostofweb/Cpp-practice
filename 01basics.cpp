831957978f673152293052aba018c748189e37f6
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-09-11 22:10:37 +0530

    ab

---
+++ 01basics.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+
+
+//  int main(){
+//     string a;
+//     a="abc";
+//     int num;
+//     cin>>num;
+//     cout<<a;
+//     cout<<num<<endl;
+//     return 0;
+//  }
+
+
+// //Pointers
+// int main(){
+//     int card = 40;
+//     int *myp;
+//     int my_card ; 
+
+//     myp = &card; // myp is a pointer which is storing the address of card
+//     my_card = *myp; //pointer deref > giving the value to the variable with its address
+
+//     printf("%p\n",myp);
+//     printf("%d\n",my_card);
+    
+// }
+
+
+// Referece
+// int main() {
+//     int score = 200;
+//     int *myp = &score;
+
+//     printf("%d\n",score);
+//     printf("%p\n",myp);
+
+//     int &another_score = score;
+//     another_score = 300;
+
+//     printf("%d\n",score);
+//     printf("%p\n",myp);
+//     printf("%d",another_score);
+// }
+
+
+// int main(){
+//     int a[4];
+//     a[0] = 10;
+//     a[1] = 20;
+
+//     cout<<a[0]<<endl;
+//     cout<<a[1]<<endl;
+
+//     *a = 40; // assigning value to the 0th index of the array
+//     cout<<a[0]<<endl;
+
+//     int *ap = a;
+//     ap++; //goint to the next index
+//     *ap = 102;
+//     cout<<a[1]<<endl;
+
+// }
+
+// CONDITIONS
+
+// int main()
+// {
+//     int a = 5;
+
+//     if (a == 5)
+//     {
+//         cout<<"AYO"<<endl;
+//     }else{
+//         printf("ato");
+//     }
+    
+//     if( true || 123 ){
+//         cout<<"it will execute if it have true or any value above 0 ";
+//     }
+
+//     if(false || 0 || NULL){
+//         cout<<"it will not execute because it have false and 0 and NULL ";
+//     }
+
+//     return 0;
+// }
+
+
+
+// //LOOPS
+// int main(int argc, char const *argv[])
+// {
+//     while (true)
+//     {
+//         /* code */
+//     }
+
+//     do
+//     {
+//         /* code */
+//     } while (true);
+
+//     for (size_t i = 0; i < 10; i++)
+//     {
+//         /* code */
+//     }
+    
+//     return 0;
+// }
+
+
---
+++ 02strings.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+// int main()
+// {
+//     char a[] = "hello world"; // the way to define a string using char
+//     cout << a <<endl;
+//     char b[] = {'f','e','d','b','a',0};
+//     cout<<b<<endl;
+
+//     for (int i = 0; i < a[i] ; i++) //looping till the array terminates
+//     {
+//         cout<<a[i];
+//     }
+//     return 0;
+
+//     for (char *cp = a; *cp != 0 ; cp++) // pointing towards the first element then looping till we get 0 by derefrencing it each time and then ++ for next index
+//     {
+//         cout<<*cp<<endl;
+//     }
+    
+// }
+
+
+// //TRY-CATCH
+
+// int main(int argc, char const *argv[])
+// {
+//     try
+//     {
+//         /* code */
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+    
+//     return 0;
+// }
+
+//Quantifires
+// static
+
+
+int lifeup(){
+    static int life = 3;
+    return ++life;
+}
+
+int main(int argc, const char** argv) {
+    int life;
+    life = lifeup();
+    life = lifeup();
+    cout<<life<<endl;
+    return 0;
+}
