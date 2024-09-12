4dbbf13fe4df8a00c891930bfccba3d4396a1e15
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-09-12 17:33:46 +0530

    recursion factorial

---
+++ 09recursion.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int fact(int a){
+    if(a > 1) {
+         return a * fact(a-1);
+    }
+}
+
+int main(){
+    int value = fact(5);
+    printf("%d", value);
+}
