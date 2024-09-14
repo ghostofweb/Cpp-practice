0ba58139ddf549a34e43c5bed40e18df834faf07
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-09-14 16:08:00 +0530

    poly

---
+++ 014polymorphism.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+class One{
+    public:
+    virtual void intro(){
+        cout<<"This is class One \n"<<endl;
+    }
+};
+
+class Two : public One{
+    public:
+    void intro(){
+        cout<<"This is class Two\n"<<endl;
+    }
+};
+
+class Three : public One{
+    public:
+    void intro(){
+        cout<<"This is class three\n"<<endl;
+    }
+};
+
+int main(){
+    One *a;// pointer of a with the datatype One
+    Two b;
+    Three c;
+   
+   a = &b;
+   a->intro(); // without virtual 
+}
