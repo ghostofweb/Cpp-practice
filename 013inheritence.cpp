85f7e90445dee031e68b0ed79a7928359d72feb8
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-09-14 15:35:20 +0530

    inheritence and multiple inheritence

--- 013inheritence.cpp
+++ 013inheritence.cpp
@@ -2,8 +2,43 @@
 
 using namespace std;
 
+class Man{
+    string _name;
+    int _age;
+    Man(){}
+    protected:
 
+    Man(const string & name, const int & age)
+    :_name(name),
+    _age(age){} // paramatric constructor
+
+    void run(){puts("i can run");}
+
+    public:
+    void sayName() const;
+};
+
+void Man::sayName() const{
+    cout << _name << endl;
+}
+
+
+class Superman : public Man{
+    bool flight;
+    public:
+    Superman(string name) : Man(name, 26){}
+    void run(){puts("fast boiii");}
+};
+
+// spider man
+class SpiderMan : public Man{
+    bool web;
+    public:
+    SpiderMan(string name) : Man(name, 25){}
+    void run(){puts("spidey sense");}
+};
 
 int main(){
-
+    Superman clark("ayo");
+    clark.sayName();
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+    clark.sayName();
